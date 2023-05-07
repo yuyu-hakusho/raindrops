5ab793bb2eea571e721e667b722311b1b5404794
Jennifer-Tsang_lmig <jennifer.tsang@libertymutual.com>
2023-05-07 12:48:16 -0400

    adding updated raindrops

--- raindrops.js
+++ raindrops.js
@@ -1,1 +1,19 @@
-export function convert() {}
+export function convert(input) {
+    let sounds = "";
+
+    if (input % 3 === 0) {
+        sounds += "Pling";
+    }
+
+    if (input % 5 === 0) {
+        sounds += "Plang";
+    }
+
+    if (input % 7 === 0) {
+        sounds += "Plong";
+    }
+
+    return sounds || String(input);
+
+}
+
